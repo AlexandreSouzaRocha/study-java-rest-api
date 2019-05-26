ed638c6a36f93c81d5610d06f1e090ac4c770605
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2019-05-26 18:21:20 -0300

    Spring Boot Dependency
    Understend Denependency on Spring Boot, no QuickSortAlgorithm

---
+++ spring-basics/src/main/java/com/in28minutes/spring/basics/springbasics/BinarySearchImpl.java
@@ -0,0 +1,19 @@
+package com.in28minutes.spring.basics.springbasics;
+
+public class BinarySearchImpl {
+
+	private SortAlgorithm sortAlgorithm;
+	
+		public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+		super();
+		this.sortAlgorithm = sortAlgorithm;
+	}
+
+		public int binarySearch(int[] numbers, int numberToSearchFor) {
+			
+			int[] sortedNumbers = sortAlgorithm.sort(numbers);
+			System.out.println(sortAlgorithm);
+			
+			return numberToSearchFor;
+		}
+}
---
+++ spring-basics/src/main/java/com/in28minutes/spring/basics/springbasics/BubbleSortAlgorithm.java
@@ -0,0 +1,24 @@
+package com.in28minutes.spring.basics.springbasics;
+
+public class BubbleSortAlgorithm implements SortAlgorithm{
+	int result;
+	int i;
+	int j;
+	int temp;
+	
+		public int[] sort(int[] numbers) {
+			
+			result = numbers.length;
+			
+			for (i = 0; i < result - 1; i++)
+	            for (j = 0; j < result - i - 1; j++)        
+	            	if (numbers[j] > numbers[j+1]) {
+	                   
+	            		temp = numbers[j];
+	                    numbers[j] = numbers[j+1];
+	                    numbers[j+1] = temp;
+	                }
+			
+			return numbers;
+		}
+}
