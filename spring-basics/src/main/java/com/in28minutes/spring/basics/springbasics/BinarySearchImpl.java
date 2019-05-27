3a5cbc6cae96156851a61460dc806a7181a55a37
Alexandre S. Rocha <alexandresouzarocha943@gmail.com>
2019-05-27 16:07:37 -0300

    Learn Spring Boot Spet 2.1
    Debug on Spring Boo Framework

--- spring-basics/src/main/java/com/in28minutes/spring/basics/springbasics/BinarySearchImpl.java
+++ spring-basics/src/main/java/com/in28minutes/spring/basics/springbasics/BinarySearchImpl.java
@@ -8,17 +8,17 @@ public class BinarySearchImpl {
 
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
-	
-		public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
+
+	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
 		super();
 		this.sortAlgorithm = sortAlgorithm;
 	}
 
-		public int binarySearch(int[] numbers, int numberToSearchFor) {
-			
-			int[] sortedNumbers = sortAlgorithm.sort(numbers);
-			System.out.println(sortAlgorithm);
-			
-			return numberToSearchFor;
-		}
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+
+		int[] sortedNumbers = sortAlgorithm.sort(numbers);
+		System.out.println(sortAlgorithm);
+
+		return numberToSearchFor;
+	}
 }
