96123166ce36e6c2587f4cd45d81ac083201421c
Alexandre S. Rocha <alexandresouzarocha943@gmail.com>
2019-05-27 15:29:04 -0300

    Learn Spring Boot Step 2
    Dependency Injection, @Component and @Autowired

--- spring-basics/src/main/java/com/in28minutes/spring/basics/springbasics/SpringBasicsApplication.java
+++ spring-basics/src/main/java/com/in28minutes/spring/basics/springbasics/SpringBasicsApplication.java
@@ -2,19 +2,21 @@ package com.in28minutes.spring.basics.springbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
 
-	private static int result;
-
 	public static void main(String[] args) {
-
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-		result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-
+		
+		ConfigurableApplicationContext applicationContext = 
+				SpringApplication.run(SpringBasicsApplication.class, args);
+		
+		BinarySearchImpl binarySearch = 
+				applicationContext.getBean(BinarySearchImpl.class);
+		
+		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
 		System.out.println(result);
-		SpringApplication.run(SpringBasicsApplication.class, args);
 	}
 
 }
