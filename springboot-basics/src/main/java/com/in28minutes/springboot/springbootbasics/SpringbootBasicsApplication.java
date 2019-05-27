9981e08d9caa419f573ab0f504b878f1934e5690
Alexandre S. Rocha <alexandresouzarocha943@gmail.com>
2019-05-27 18:58:11 -0300

    Spring Boot Basics
    Learn the basics of Spring Boot

---
+++ springboot-basics/src/main/java/com/in28minutes/springboot/springbootbasics/SpringbootBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.springbootbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicsApplication.class, args);
+	}
+
+}
