9c836fe694d2160b750f6e0a7f7a262f8a28878c
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2019-05-28 09:09:22 -0300

    Spring Boot Bascis Step 2
    Learn the AutoConfiguration of Spring Boot on the Fackage os web on jars of Spring Booot

--- springboot-basics/src/main/java/com/in28minutes/springboot/springbootbasics/SpringbootBasicsApplication.java
+++ springboot-basics/src/main/java/com/in28minutes/springboot/springbootbasics/SpringbootBasicsApplication.java
@@ -2,12 +2,19 @@ package com.in28minutes.springboot.springbootbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootBasicsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBasicsApplication.class, args);
+		
+		ApplicationContext applicationContext = 
+				SpringApplication.run(SpringbootBasicsApplication.class, args);
+		
+		for (String name: applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
