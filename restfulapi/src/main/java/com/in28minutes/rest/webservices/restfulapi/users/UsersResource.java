083a3bee61f95c9692bcae52168cfcc46f95a02f
Alexandre S. Rocha <alexandresouzarocha943@gmail.com>
2019-05-29 16:22:48 -0300

    Update UsersResource.java

--- restfulapi/src/main/java/com/in28minutes/rest/webservices/restfulapi/users/UsersResource.java
+++ restfulapi/src/main/java/com/in28minutes/rest/webservices/restfulapi/users/UsersResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +18,8 @@ public class UsersResource {
 		return services.findAllUsers();
 	}
 
-/*	@GetMapping("/users/find-one")
+	@GetMapping("/users/find-one")
 	public User retrieveOneUser(@RequestParam int id) {
 		return services.findOneUser(id);
-	}*/
+	}
 }
