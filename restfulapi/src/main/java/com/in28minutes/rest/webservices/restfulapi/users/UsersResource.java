9c1427b8d9f6aaf735279676ff68005a45e5460d
Alexandre S. Rocha <alexandresouzarocha943@gmail.com>
2019-05-29 16:55:53 -0300

    Spring Boot Rest API Debbuging
    No modificated

--- restfulapi/src/main/java/com/in28minutes/rest/webservices/restfulapi/users/UsersResource.java
+++ restfulapi/src/main/java/com/in28minutes/rest/webservices/restfulapi/users/UsersResource.java
@@ -4,22 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersResource {
 
 	@Autowired
-	private UserDAOService services;
+	private UserDAOService service;
 
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
-		return services.findAllUsers();
+		return service.findAllUsers();
 	}
 
-	@GetMapping("/users/find-one")
-	public User retrieveOneUser(@RequestParam int id) {
-		return services.findOneUser(id);
-	}
+	/*
+	 * @GetMapping("/users/find-one/{id}") public User retrieveOneUser(@PathVariable
+	 * int id) { return services.findOneUser(id); }
+	 */
 }
