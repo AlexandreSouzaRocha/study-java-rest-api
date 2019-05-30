329503aff8618a133de01fca8e6a8539dad32745
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2019-05-29 22:17:18 -0300

    Spring Probelms
    Testing

--- restfulapi/src/main/java/com/in28minutes/rest/webservices/restfulapi/resource/RestfulapiApplication.java
+++ restfulapi/src/main/java/com/in28minutes/rest/webservices/restfulapi/resource/RestfulapiApplication.java
@@ -1,9 +1,11 @@
-package com.in28minutes.rest.webservices.restfulapi.helloword;
+package com.in28minutes.rest.webservices.restfulapi.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class RestfulapiApplication {
 
 	public static void main(String[] args) {
--- restfulapi/src/main/java/com/in28minutes/rest/webservices/restfulapi/resource/UsersResource.java
+++ restfulapi/src/main/java/com/in28minutes/rest/webservices/restfulapi/resource/UsersResource.java
@@ -7,14 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.rest.webservices.restfulapi.service.UserDAOService;
 import com.in28minutes.rest.webservices.restfulapi.users.User;
 
 @RestController
-@RequestMapping(value = "/restapi")
 public class UsersResource {
 
 	@Autowired
