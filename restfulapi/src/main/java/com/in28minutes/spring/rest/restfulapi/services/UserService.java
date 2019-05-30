01aa291201b2b6006ac7ed1825d7a7a47c0712e9
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2019-05-30 00:00:37 -0300

    Project Tested and Runnig
    Tested and Runnig on Get method

--- restfulapi/src/main/java/com/in28minutes/spring/rest/restfulapi/services/UserService.java
+++ restfulapi/src/main/java/com/in28minutes/spring/rest/restfulapi/services/UserService.java
@@ -1,6 +1,7 @@
 package com.in28minutes.spring.rest.restfulapi.services;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -12,6 +13,12 @@ public class UserService {
 
 	private static int userCount = 3;
 	private static List<User> usersList = new ArrayList<>();
+	
+	static {
+		usersList.add(new User(1, "Alexandre", new Date()));
+		usersList.add(new User(2, "José", new Date()));
+		usersList.add(new User(3, "João", new Date()));
+	}
 
 	public List<User> findAllUsers() {
 		return usersList;
