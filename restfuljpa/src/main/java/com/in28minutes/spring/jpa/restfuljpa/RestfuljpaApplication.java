d5792277820b4a422b97574055dfcad93c609ac9
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2019-06-16 17:42:57 -0300

    Spring with JPA and Database
    First step of the project : Spring boot with JPA + Database

---
+++ restfuljpa/src/main/java/com/in28minutes/spring/jpa/restfuljpa/RestfuljpaApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.jpa.restfuljpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfuljpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfuljpaApplication.class, args);
+	}
+
+}
---
+++ restfuljpa/src/test/java/com/in28minutes/spring/jpa/restfuljpa/RestfuljpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in28minutes.spring.jpa.restfuljpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfuljpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
