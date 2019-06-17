3adf41df130c88e9045bb4e62e8a680d19883fc3
Alexandre S. Rocha <alexandresouzarocha943@gmail.com>
2019-06-17 16:43:18 -0300

    Spring Boot JPA Repository
    Starts with JPA Repository and CommandLineRunner

---
+++ restfuljpa/src/main/java/com/in28minutes/spring/jpa/restfuljpa/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.in28minutes.spring.jpa.restfuljpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.in28minutes.spring.jpa.restfuljpa.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
